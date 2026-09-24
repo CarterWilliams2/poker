3c00ebf73337c7379e3591dc81da5a0fc78ff351
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-09-23 20:48:40 -0500

    make basic card class

---
+++ Card.py
@@ -0,0 +1,11 @@
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def getValue(self):
+        return self.value
+    
+    def getSuit(self):
+        return self.suit
+
